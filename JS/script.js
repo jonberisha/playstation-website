be808cbec884e3696ee2c0ec8492a15687085a17
Jon Berisha <jonberisha08@gmail.com>
2021-06-10 14:16:16 +0200

    added a footer and submit email form

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,21 @@
-// function testFunc(){
-// for(var i = 0; i < imgSource.length; i++){
-// function sliderFunc(){
-//     console.log('working');
-//     var imgURL = imgPath + imgSource[i]
-//     var bg = document.getElementById('background-slideshow');
-//     bg.style.backgroundImage = `url(${imgURL})`;
-// }
-//     setTimeout(function(){
-//         sliderFunc();
-//     },2000);
-//     }
-// }
-// testFunc();
-
 function onClickButton() {
-    console.log("test");
-  }
-  
-  
-  
-  
-  
+  console.log("test");
+}
+
 var imgSource = [
-    "featured-games",
-    "Playstation-5-games-console-transparent-background-png-image.webp",
-  ];
-var imgPath = '/home/jon/Desktop/github/playstation-website/assets/';
+  "featured-games",
+  "Playstation-5-games-console-transparent-background-png-image.webp",
+];
+var imgPath = "/home/jon/Desktop/github/playstation-website/assets/";
 
 // Background Image Slideshow Function
 var i = 0;
-setInterval(function(){
-    var imgURL = imgPath + imgSource[i]
-    var bg = document.getElementById('background-slideshow');
-    bg.style.backgroundImage = `url(${imgURL})`;
-    i = i + 1;
-    if(i == imgSource.length){
-        i = 0;
-    }
+setInterval(function () {
+  var imgURL = imgPath + imgSource[i];
+  var bg = document.getElementById("background-slideshow");
+  bg.style.backgroundImage = `url(${imgURL})`;
+  i = i + 1;
+  if (i == imgSource.length) {
+    i = 0;
+  }
 }, 2000);
