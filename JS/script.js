cb2919104ac02a3db79bca60bf4e4e633835d56c
Jon Berisha <jonberisha08@gmail.com>
2021-06-10 11:53:51 +0200

    created background-image slideshow. Added a new default button on '4k' section

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,40 @@
-var el = document.querySelector('button')
+// function testFunc(){
+// for(var i = 0; i < imgSource.length; i++){
+// function sliderFunc(){
+//     console.log('working');
+//     var imgURL = imgPath + imgSource[i]
+//     var bg = document.getElementById('background-slideshow');
+//     bg.style.backgroundImage = `url(${imgURL})`;
+// }
+//     setTimeout(function(){
+//         sliderFunc();
+//     },2000);
+//     }
+// }
+// testFunc();
 
-el.addEventListener('click', testFunc);
+function onClickButton() {
+    console.log("test");
+  }
+  
+  
+  
+  
+  
+var imgSource = [
+    "featured-games",
+    "Playstation-5-games-console-transparent-background-png-image.webp",
+  ];
+var imgPath = '/home/jon/Desktop/github/playstation-website/assets/';
 
-function testFunc() {
-    console.log('test');
-}
+// Background Image Slideshow Function
+var i = 0;
+setInterval(function(){
+    var imgURL = imgPath + imgSource[i]
+    var bg = document.getElementById('background-slideshow');
+    bg.style.backgroundImage = `url(${imgURL})`;
+    i = i + 1;
+    if(i == imgSource.length){
+        i = 0;
+    }
+}, 2000);
